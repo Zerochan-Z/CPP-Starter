50b26e310b6b6d938917d810c8d142daa1a3690a
Zerochan-Z <boonfeung08@gmail.com>
2026-06-28 22:29:15 +0800

    Repetition day 1 (20 %)

--- Projects 001 (1st Year & 1st Trimester)/Library_management.cpp
+++ Projects 001 (1st Year & 1st Trimester)/Library_management.cpp
@@ -2,21 +2,21 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cctype>
 #include <limits>
+#include <cctype>
 #include <iomanip>
 using namespace std;
 
-string toTitle(string &str) {
+string toTitle (string &str) {
     string result = str;
-    bool Upper = true;
+    bool upper = true;
 
-    for (size_t i = 0; i < result.length(); i++ ) {
+    for (size_t i = 0 ; i < result.length(); i++) {
         if (isspace(result[i])) {
-            Upper = true;
-        } else if (Upper) {
+            upper = true;
+        } else if (upper) {
             result[i] = toupper(result[i]);
-            Upper = false;
+            upper = false;
         } else result[i] = tolower(result[i]);
     }
     return result;
@@ -24,10 +24,10 @@ string toTitle(string &str) {
 
 string toLower(string &str) {
     string result = str;
-
     for (size_t i = 0; i < result.length(); i++) {
         result[i] = tolower(result[i]);
     }
+
     return result;
 }
 
@@ -38,7 +38,7 @@ string StatusStr(char status) {
     else return "Missing";
 }
 
-int StatusNum (char status) {
+int StatusInt(char status) {
     if (status == 'A') return 1;
     if (status == 'B') return 2;
     if (status == 'R') return 3;
@@ -46,113 +46,135 @@ int StatusNum (char status) {
 }
 
 class Book {
-    private: 
-        string title, author;
-        int year;
-        char status;
-    
-    public:
+private:
+    string title;
+    string author;
+    int year;
+    char status;
 
-        Book() : title(""), author(""), year(0), status('A') {}
-        Book (string t, string a, int y, char s) : title(t), author(a), year(y), status(s) {}
+public:
 
-        string getName() { return title; }
-        string getAuthor() { return author; }
-        int getYear() { return year; }
-        char getStatus() { return status; }
+    Book() : title(""), author(""), year(0), status('A') {}
+    Book(string t, string a, int y, char s) : title(t), author(a), year(y), status(s) {}
 
-        void setName(string t) { 
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore();
-                cout << "Invalid input.\n";
-            } else title = t;
-        }
-        void setAuthor (string a) { 
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore();
-                cout << "Invalid input.\n";
-            } else author = a;
-        }
-        void setYear (int y) { 
-            if (y > 0 && y < 2027) {
-                year = y;
-            } else {
-                cin.clear();
-                cin.ignore();
-                cout << "Invalid input.\n";
-            }
-        }
-        void setStatus (char s) {
-            if (cin.fail()) {
-                cin.clear();
-                cin.ignore();
-                cout << "Please enter an alphabet.\n A - Availble   B - Borrowed    R - Reserved\n";
-            } else status = toupper(s);
-        }
+    string getName() { return title; }
+    string getAuthor() { return author; }
+    int getYear() { return year; }
+    char getStatus() { return status; }
 
-        void display() {
-            cout << left << setw(25) << toTitle(name)
-                 << setw(20) << toTitle(author)
-                 << setw(8) << year
-                 << setw(12) << StatusStr(status) << endl;
-        }
+    void setName(string t) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid input.\n";
+        } else title = t;
+    }
 
-        void saveFile(ofstream &outFile) {
-            outFile << title << endl;
-            outFile << author << endl;
-            outFile << year << endl;
-            outFile << status << endl;
-        }
+    void setAuthor(string a) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid input.\n";
+        } else author = a;
+    }
 
-        void loadFile(ifstream &inFile) {
-            getline(inFile, title);
-            getline(inFile, author);
-            inFile >> year;
-            inFile >> status;
-            inFile.ignore();
-        }
+    void setYear (int y) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid input.\n";
+        } else year = y;
+    }
+
+    void setStatus (char s) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Invalid input.\n"
+        } else if ( s == 'A' || s == 'B' || s == 'R' || s == ' ') status = s;
+        else cout << "A - Available, B - Borrowed, R - Reserved.\n";
+    }
+
+    void display() {
+        cout << left << setw(25) << toTitle(title)
+             << setw(20) << toTitle(author)
+             << setw(8) << year
+             << setw(12) << StatusStr(status) << endl;
+    }
+
+    void saveFile(ofstream &outFile) {
+        outFile << title << endl;
+        outFile << author << endl;
+        outFile << year << endl;
+        outFile << status << endl;
+    }
+
+    void loadFile(ifstream &inFile) {
+        getline(inFile, title);
+        getline(inFile, author);
+        inFile >> year;
+        inFile >> status;
+        inFile.ignore();
+    }
 };
 
 class Library {
-    private: 
-        vector <Book> book;
+private: 
+    vector <Book> book;
 
-    public: 
-        void saveFile() {
-            ofstream outFile("books.txt");
-            if (!outFile) {
-                cout << "Error.\n";
-                return;
-            }
+public:
 
-            for (size_t i = 0; i < book.size(); i++) {
-                book[i].saveFile(outFile);
-            }
-
-            outFile.close();
+    void saveFile() {
+        ofstream outFile ("books.txt");
+        if (!outFile) {
+            cout << "No book  saved.\n";
+            return;
         }
 
-        void loadFile() {
-            ifstream inFile("books.txt");
-            if (!inFile) {
-                cout << "Error.\n";
-                return;
-            }
+        for (size_t i = 0; i < book.size(); i++) book[i].saveFile(outFile);
 
-            Book b;
-            while (!inFile.eof()) {
-                b.loadFile(inFile);
-                book.push_back(b);
-            }
-            inFile.close();
+        outFile.close();
+        cout << book.size() << " book(s) saved.\n";
+    }
+
+    void loadFile() {
+        ifstream inFile("books.txt");
+        if (!inFile) {
+            cout << "No book loaded.\n";
+            return;
         }
 
-        void addBook() {
-            Book b;
-            string title, author;
-            int year;
-            char status;
+        Book b;
+        while (!inFile.eof()) {
+            b.loadFile(inFile);
+            book.push_back(b);
         }
+
+        inFile.close();
+        cout << book.size() << " book loaded.\n";
+    }
+
+    void addBook() {
+        Book b;
+        string title, author;
+        int year;
+        char status;
+
+        cout << "Enter book title: ";
+        cin.ignore();
+        getline(cin, title);
+        b.setName(title);
+        cout << "Enter book author: ";
+        getline(cin, author);
+        b.setAuthor(author);
+        cout << "Enter year: ";
+        cin >> year;
+        b.setYear(year);
+        cout << "Enter status: ";
+        cin >> status;
+        b.setStatus(status);
+
+        book.push_back(b);
+        saveFile();
+    }
 }
